cf404eaeecd3e759e80a22ed67d982d5d1eacee0
Sam Chappel <m.samchappel@gmail.com>
2023-01-24 21:21:17 -0800

    likes and like button functionality added and working

--- index.js
+++ index.js
@@ -5,74 +5,31 @@ const endUrl = "&date=today"
 const sunriseList = document.querySelector('#sunrise-list')
 const parksUrl = 'http://localhost:3000/parks'
 
-const yosemiteURL= 'https://api.sunrisesunset.io/json?lat=37.84835&lng=-119.55696&timezone=PST'
-getParks(parksUrl)
+// FETCH FUNCTIONS
 function getParks(parksUrl){
     fetch(parksUrl)
     .then(r => r.json())
     .then(parkArray => {
         const sunnyParks = parkArray.map(park => getAllSunrises(park.lat, park.long, park.tmz, park))
         // console.log(sunnyParks)
-        testFunction(sunnyParks)
     })
 }
-// function getSunset(park){
-//     return fetch(yosemiteURL)
-//     .then(res => res.json())
-//     .then(sunsetData => {
-//         // console.log(sunsetData)
-//         renderNationalParks(park, sunsetData.results)
-//         return {...park,
-//         sunsetData
-//         // sunrise: sunsetData.results.sunrise,
-//         // sunset: sunsetData.results.sunset
-//     }
-//     })
-// }
-function testFunction(parksArray){
-    // console.log(parksArray[5])
-    // parksArray.forEach(console.log(parksArray))
-}
 
-// FETCH FUNCTIONS
 function getAllSunrises(lat, lng, tmz, park) {
     return fetch(baseUrl + `lat=${lat}&lng=${lng}&timezone=${tmz}` + endUrl)
         .then(response => response.json())
         .then((sunsetData) => renderNationalParks(park, sunsetData.results))
 }
 
-function getLocationData(url) {
-    return fetch('url')
-    .then(response => response.json())
-}
+// function getLocationData(url) {
+//     return fetch('url')
+//     .then(response => response.json())
+// }
 
 function getNationalParks(url) {
     return fetch(url)
         .then(response => response.json())
 }
-
-// function getNationalParks(url){
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(parksArray => {
-//         const sunnyParks = parksArray.map(park => getAllSunrises(park))
-//         iterateParks(sunnyParks)
-//     })
-// }
-
-// function getAllSunrises(park) {
-//     return fetch(baseUrl + `lat=${park.lat}&lng=${park.long}&timezone=${park.tmz}` + endUrl)
-//     .then(response => response.json())
-//     .then(sunsetData => {
-//         return{...park,
-//         "Sunrise": sunsetData.results.sunrise,
-//         "Sunset": sunsetData.results.sunset,
-//         "Golden Hour": sunsetData.results["golden_hour"],
-//         "Day Length": sunsetData.results["day_length"]
-//     }
-//     })
-// }
-
 
 // RENDER FUNCTIONS
 function renderNationalParks(park, sunsetData) {
@@ -104,23 +61,27 @@ function renderNationalParks(park, sunsetData) {
     const dayLength = document.createElement('p')
     dayLength.textContent = park["day_length"]
     const dayLengthSpan = document.createElement('span')
-    dayLengthSpan.textContent = sunsetData.day_length
+    dayLengthSpan.textContent = sunsetData.day_length + " hours"
     dayLength.append(dayLengthSpan)
+    const likes = document.createElement('h5')
+    likes.textContent = park.likes + " likes"
     const like = document.createElement('button')
     like.textContent = "Like ☀"
     sunriseList.append(li)
-    li.append(image, nationalPark, location, sunrise, sunset, goldenHour, dayLength, like)
+    li.append(image, nationalPark, location, sunrise, sunset, goldenHour, dayLength, likes, like)
+    like.addEventListener('click', () => {
+        console.log('clicked')
+        const newLikes = park.likes + 1;
+        park.likes = newLikes
+        likes.textContent = park.likes + ' likes'
+    })
 }
 
-function iterateParks(parksArray) {
-    parksArray.forEach(renderNationalParks)
-}
 
-// function iterateParks(sunnyParks) {
-//     sunnyParks.forEach(park => renderNationalParks(park))
+
+// function iterateParks(parksArray) {
+//     parksArray.forEach(renderNationalParks)
 // }
 
 // INITIALIZERS
-// getNationalParks(parksUrl).then(parksArray => {iterateParks(parksArray)})
-// getNationalParks(parksUrl)
-// getAllSunrises("37.865101", "-119.538330", "PST")
+getParks(parksUrl)
