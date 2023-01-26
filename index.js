f31d366f29c2b2580a1d9958c69678abca86532f
Sam Chappel <m.samchappel@gmail.com>
2023-01-25 16:35:43 -0800

    submit form working, reset not currently working. also updated parks json object and renderNationalPark function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const baseUrl = "https://api.sunrisesunset.io/json?"
 const endUrl = "&date=today"
 const sunriseList = document.querySelector('#sunrise-list')
 const parksUrl = 'http://localhost:3000/parks'
+const sunForm = document.querySelector('#sun-form');
 
 // FETCH FUNCTIONS
 function getParks(parksUrl){
@@ -39,36 +40,24 @@ function renderNationalParks(park, sunsetData) {
     li.className = "list-li"
     const image = document.createElement('img')
     image.src = park.image
-    const nationalPark = document.createElement('h3')
-    nationalPark.textContent = park.nationalPark
-    const location = document.createElement('h4')
+    const location = document.createElement('h3')
     location.textContent = park.location
+    const cityState = document.createElement('h4')
+    cityState.textContent = park.cityState
     const sunrise = document.createElement('p')
-    sunrise.textContent = park.sunrise
-    const sunriseSpan = document.createElement('span')
-    sunriseSpan.textContent = sunsetData.sunrise
-    sunrise.append(sunriseSpan)
+    sunrise.textContent = 'Sunrise: ' + sunsetData.sunrise
     const sunset = document.createElement('p')
-    sunset.textContent = park.sunset
-    const sunsetSpan = document.createElement('span')
-    sunsetSpan.textContent = sunsetData.sunset
-    sunset.append(sunsetSpan)
+    sunset.textContent = 'Sunset: ' + sunsetData.sunset
     const goldenHour = document.createElement('p')
-    goldenHour.textContent = park["golden_hour"]
-    const goldenHourSpan = document.createElement('span')
-    goldenHourSpan.textContent = sunsetData.golden_hour
-    goldenHour.append(goldenHourSpan)
+    goldenHour.textContent = 'Golden Hour: ' + sunsetData.golden_hour
     const dayLength = document.createElement('p')
-    dayLength.textContent = park["day_length"]
-    const dayLengthSpan = document.createElement('span')
-    dayLengthSpan.textContent = sunsetData.day_length + " hours"
-    dayLength.append(dayLengthSpan)
+    dayLength.textContent = 'Day Length: ' + sunsetData.day_length + ' Hours'
     const likes = document.createElement('h5')
-    likes.textContent = park.likes + " likes"
+    likes.textContent = park.likes + " Likes"
     const like = document.createElement('button')
     like.textContent = "Like ☀"
     sunriseList.append(li)
-    li.append(image, nationalPark, location, sunrise, sunset, goldenHour, dayLength, likes, like)
+    li.append(image, location, cityState, sunrise, sunset, goldenHour, dayLength, likes, like)
     like.addEventListener('click', () => {
         console.log('clicked')
         const newLikes = park.likes + 1;
@@ -77,7 +66,27 @@ function renderNationalParks(park, sunsetData) {
     })
 }
 
-
+sunForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // debugger
+    const lat = e.target[3].value
+    const long = e.target[4].value
+    const tmz = e.target[5].value
+    const image = e.target[0].value
+    const location = e.target[1].value
+    const cityState = e.target[2].value
+    const newCardObj = {
+        image,
+        location,
+        lat,
+        long,
+        tmz,
+        cityState,
+        likes: 1
+    }
+    renderNationalParks(getAllSunrises(lat, long, tmz, newCardObj))
+    sunForm.reset()
+})
 
 // function iterateParks(parksArray) {
 //     parksArray.forEach(renderNationalParks)
